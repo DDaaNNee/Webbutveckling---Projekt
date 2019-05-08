fa308b18041aae19e5569cd48c7d6e3edac75540
DDaaNNee <50401918+DDaaNNee@users.noreply.github.com>
2019-05-08 15:16:14 +0200

    Update

--- process.php
+++ process.php
@@ -6,21 +6,6 @@
 
     addToDatabase($namnet);
   }
-?>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>Länkar till andra sidor</h1>
-    <h2><a href="index.html">Index.html</h2>
-    <h2><a href="form.html">Form.html</h2>
-    <h2><a href="process.php">Process.php</h2>
-
-  </body>
-</html>
-<?php
 function addToDatabase($namn){
 
 }
